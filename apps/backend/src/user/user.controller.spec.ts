afb1c11414523b1bb03dd228905a7547159ee556
Spas Milenkov <spasmilenkov123@gmail.com>
2024-08-25 23:41:43 +0300

    fix: fix testing pipeline

--- apps/backend/src/user/user.controller.spec.ts
+++ apps/backend/src/user/user.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { DatabaseModule } from '../database/database.module';
+import { JwtModule } from '@nestjs/jwt';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -9,6 +11,14 @@ describe('UserController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
       providers: [UserService],
+      imports: [
+        DatabaseModule,
+        JwtModule.register({
+          secret: process.env.JWT_KEY,
+          secretOrPrivateKey: process.env.JWT_KEY,
+          signOptions: { expiresIn: '1h' },
+        }),
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);

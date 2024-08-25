6c4a96a4ba440f9910f2ad521e94101f9d88b35b
Spas Milenkov <spasmilenkov123@gmail.com>
2024-08-25 23:18:29 +0300

    feat: add user module

---
+++ apps/backend/src/user/user.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { DatabaseModule } from 'src/database/database.module';
+
+@Module({
+  controllers: [UserController],
+  providers: [UserService],
+  imports: [
+    DatabaseModule,
+    JwtModule.register({
+      secret: process.env.JWT_KEY,
+      secretOrPrivateKey: process.env.JWT_KEY,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  exports: [UserService],
+})
+export class UserModule {}
---
+++ apps/backend/src/user/user.service.ts
@@ -0,0 +1,69 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { DatabaseService } from 'src/database/database.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(
+    private prisma: DatabaseService,
+    private jwtService: JwtService
+  ) {}
+
+  async register(data: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        password: hashedPassword,
+      },
+    });
+
+    return this.generateToken(user);
+  }
+
+  async login(data: { email: string; password: string }) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (!user || !(await bcrypt.compare(data.password, user.password))) {
+      throw new BadRequestException('Invalid email or password');
+    }
+
+    return this.generateToken(user);
+  }
+
+  async update(userId: string, data: UpdateUserDto) {
+    const updateData: any = {};
+
+    if (data.name) updateData.name = data.name;
+    if (data.email) updateData.email = data.email;
+    if (data.password)
+      updateData.password = await bcrypt.hash(data.password, 10);
+
+    const updatedUser = await this.prisma.user.update({
+      where: { id: userId },
+      data: updateData,
+    });
+
+    return updatedUser;
+  }
+
+  async delete(userId: string) {
+    return this.prisma.user.delete({
+      where: { id: userId },
+    });
+  }
+
+  private generateToken(user: any) {
+    const payload = { sub: user.id, email: user.email };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
